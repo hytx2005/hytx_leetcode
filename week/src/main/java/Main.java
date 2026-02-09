3a7c650b407dde443e90d2800fa340e4de62fd74
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2026-02-09 09:57:44 +0800

    2026.2.9 / 更改学习计划，按照二哥的计划进行算法训练

--- week/src/main/java/Main.java
+++ week/src/main/java/Main.java
@@ -1,58 +1,25 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author ASUS
  */
 public class Main implements Serializable{
-    public class Student implements Serializable {
-        String name;
-        int age;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public Student(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        Student student = new Main().new Student("Alice", 20);
-
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("student.ser"));
-        try {
-            oos.writeObject(student);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }finally {
-            oos.close();
-        }
-
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("student.ser"));
-        try {
-            try {
-                Object o = ois.readObject();
-                Student deserializedStudent = (Student) o;
-                System.out.println("Name: " + deserializedStudent.getName());
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (IOException | RuntimeException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) {
+        List<String> stringCollection = new ArrayList<>();
+        stringCollection.add("ddd2");
+        stringCollection.add("aaa2");
+        stringCollection.add("bbb1");
+        stringCollection.add("aaa1");
+        stringCollection.add("bbb3");
+        stringCollection.add("ccc");
+        stringCollection.add("bbb2");
+        stringCollection.add("ddd1");
+        stringCollection
+                .stream()
+                .map(String::toUpperCase)
+                .sorted((a, b) -> b.compareTo(a))
+                .forEach(System.out::println);
     }
 }
