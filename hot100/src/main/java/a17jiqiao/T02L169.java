4165f68c98f9619bd51351bd1b13bee99c16814b
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2026-03-20 10:00:13 +0800

    2026.03.20 L169 多数元素，可使用投票算法完成，达到空间复杂度O(1)

---
+++ hot100/src/main/java/a17jiqiao/T02L169.java
@@ -0,0 +1,19 @@
+package a17jiqiao;
+
+public class T02L169 {
+    public int test1(int[] nums){
+        int candidate = nums[0];
+        int count = 1;
+        for (int i = 1; i< nums.length;i++){
+            if(count == 0){
+                candidate = nums[i];
+                count = 1;
+            }else if(nums[i] == candidate){
+                count++;
+            }else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
