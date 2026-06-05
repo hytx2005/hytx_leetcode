e17259f56471ef5971f274c14c200f9355cbbaf1
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2026-06-05 19:20:51 +0800

    2026.06.05

---
+++ codetop/src/main/java/month202606/day05/L56.java
@@ -0,0 +1,28 @@
+package month202606.day05;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class L56 {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> res = new ArrayList<>();
+        int tBegin = intervals[0][0];
+        int tEnd = intervals[0][1];
+        int m = intervals.length;
+        for (int i = 1; i < m; i++) {
+            if(intervals[i][0] <= tEnd){
+                tBegin = Math.min(tBegin,intervals[i][0]);
+                tEnd = Math.max(tEnd,intervals[i][1]);
+            }else {
+                res.add(new int[]{tBegin,tEnd});
+                tBegin = intervals[i][0];
+                tEnd = intervals[i][1];
+            }
+        }
+        res.add(new int[]{tBegin,tEnd});
+        return  res.toArray(new int[res.size()][]);
+    }
+}
---
+++ codetop/src/main/java/month202606/day05/L72.java
@@ -0,0 +1,36 @@
+package month202606.day05;
+
+public class L72 {
+    class Solution {
+        public int minDistance(String word1, String word2) {
+            int m = word1.length();
+            int n = word2.length();
+            // dp[i][j] 表示word1[0,i]到word2[0,j]的最短编辑距离
+            int[][] dp = new int[m + 1][n + 1];
+            if (m * n == 0) {
+                return n + m;
+            }
+            // 初始化边界值
+            for (int i = 0; i <= m; i++) {
+                dp[i][0] = i;
+            }
+            for (int j = 0; j <= n; j++) {
+                dp[0][j] = j;
+            }
+            for (int i = 1 ; i <= m ; i++){
+                for(int j = 1 ; j <= n ; j++){
+                    int add1 = dp[i-1][j] + 1;
+                    int add2 = dp[i][j-1] + 1;
+                    int edit =  dp[i-1][j-1];
+                    if(word1.charAt(i-1) != word2.charAt(j-1)){
+                        edit += 1;
+                    }
+                    dp[i][j] = Math.min(edit,Math.min(add1,add2));
+                }
+            }
+            return dp[m][n];
+        }
+    }
+
+
+}
