9e56a552b6b902289adcf6add5a7a2ac438942e1
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2026-06-08 18:59:13 +0800

    2026.06.06

---
+++ codetop/src/main/java/month202606/day06/L42.java
@@ -0,0 +1,25 @@
+package month202606.day06;
+
+public class L42 {
+    class Solution {
+        public int trap(int[] height) {
+            int left = 0;
+            int right = height.length - 1;
+            int leftMax = height[0];
+            int rightMax = height[right];
+            int res = 0;
+            while (left <= right){
+                leftMax = Math.max(leftMax,height[left]);
+                rightMax = Math.max(rightMax,height[right]);
+                if(leftMax < rightMax){
+                    res += leftMax - height[left];
+                    left++;
+                }else {
+                    res += rightMax - height[right];
+                    right--;
+                }
+            }
+            return res;
+        }
+    }
+}
