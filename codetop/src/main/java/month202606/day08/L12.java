d426a332d3b90a25eb57df15ca0f6e502b36bac6
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2026-06-09 22:09:05 +0800

    2026.06.08

---
+++ codetop/src/main/java/month202606/day08/L12.java
@@ -0,0 +1,45 @@
+package month202606.day08;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class L12 {
+    class Solution {
+        Map<Integer, Integer> map;
+
+        public List<String> letterCombinations(String digits) {
+            map = Map.of(
+                    1, 0,
+                    2, 3,
+                    3, 6,
+                    4, 9,
+                    5, 12,
+                    6, 15,
+                    7, 19,
+                    8, 22,
+                    9, 26
+            );
+            List<String> res = new ArrayList<>();
+            StringBuilder temp = new StringBuilder();
+            char[] sc = digits.toCharArray();
+            allSort(res, temp, sc, 0, sc.length);
+            return null;
+        }
+
+        public void allSort(List<String> res, StringBuilder temp, char[] sc, int index, int end) {
+            if (index == end) {
+                res.add(temp.toString());
+                return;
+            }
+            int begin = map.get(sc[index] - '0' - 1);
+            int endIndex = map.get(sc[index] - '0');
+            for (int i = begin; i < endIndex; i++) {
+                temp.append((char) ('a' + i));
+                allSort(res, temp, sc, index + 1, end);
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        }
+    }
+}
