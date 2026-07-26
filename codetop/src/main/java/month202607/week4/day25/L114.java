2205843afb4c6e58b9ae42dffa62c05ccd349a3b
红柚子 <14211436+redandorange@user.noreply.gitee.com>
2026-07-26 13:36:04 +0800

    2026.07.25

---
+++ codetop/src/main/java/month202607/week4/day25/L114.java
@@ -0,0 +1,27 @@
+package month202607.week4.day25;
+
+import month202607.week4.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L114 {
+    public void flatten(TreeNode root) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
+}
